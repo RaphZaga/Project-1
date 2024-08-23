29c1a3b16087df422464bd362ddbd8550d398fc3
Raphael Enyinna <enyinnaraphael@yahoo.com>
2024-08-23 18:16:15 +0200

    implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,32 @@
+import MobileNav from "@/components/ui/MobileNav";
+import SideBar from "@/components/ui/SideBar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Raphael", lastName: "Enyinna" };
+
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+      <SideBar user={loggedIn} />
+
+      <div className="flex flex-col flex-1">
+        <div className="flex items-center justify-between p-4">
+          <Image 
+            src="/logo.svg"
+            width={30}
+            height={30}
+            alt="logo"
+          />
+          <MobileNav user={loggedIn} />
+        </div>
+        <div className="flex-1 overflow-auto">
+          {children}
+        </div>
+      </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import RightSidebar from "@/components/RightSidebar"
+import HeaderBox from "@/components/ui/HeaderBox"
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox"
 
 const Home = () => {
+const loggedIn = {firstName: "Raphael", lastName: 'Enyinna', email:"raphenyinna@gmail.com" }
+
   return (
-    <div> Home</div>
+    <section className='home'> 
+  <div className='home-content'>
+    <header className='home-header'>
+   <HeaderBox
+   type="greeting"
+   title="Welcome"
+   user={loggedIn?.firstName || "Guest"}
+   subtext="Access and manage your account and transactions efficiently"
+   />
+   <TotalBalanceBox
+   accounts={[]}
+   totalBanks={1}
+   totalCurrentBalance={1250.35}
+   />
+    </header>
+
+  </div>
+  <RightSidebar
+  user={loggedIn}
+  transactions={[]}
+  banks={[{currentBalance: 123.50}, 
+    {currentBalance: 300.32}]}
+  />
+    </section>
   )
 }
 
